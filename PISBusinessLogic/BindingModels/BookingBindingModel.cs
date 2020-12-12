370d34318d22e6a5b3fa377f57b286499898d2ba
Михаил <@DESKTOP-5331CCC>
2020-12-13 00:33:32 +0400

    Функция брони

--- PISBusinessLogic/BindingModels/BookingBindingModel.cs
+++ PISBusinessLogic/BindingModels/BookingBindingModel.cs
@@ -10,10 +10,12 @@ namespace PISBusinessLogic.BindingModels
     public class BookingBindingModel
     {
         [DataMember]
-        public int Id { get; set; }
+        public int? Id { get; set; }
         [DataMember]
         public DateTime DateFrom { get; set; }
         [DataMember]
         public DateTime DateTo { get; set; }
+        public int BookId { get; set; }
+        public int LibraryCardId { get; set; }
     }
 }
